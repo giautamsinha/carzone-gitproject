1a157414414dd8575b02e15880e7792a9b190494
giautamsinha <gautamsinha1122@gmail.com>
2022-04-02 20:15:27 +0530

    complete Team model 2

--- pages/admin.py
+++ pages/admin.py
@@ -7,7 +7,7 @@ from django.utils.html import format_html
 
 class TeamAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
-        return format_html('<img src="{}" width="40" style="border-radius: 10px;"/>'.format(object.photo.url))
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;"/>'.format(object.photo.url))
     thumbnail.short_description = 'photo'
     list_display = ('id', 'thumbnail', 'first_name',
                     'designation', 'created_date')
