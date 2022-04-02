6612ece1af0a792a76aa5ca921421b0943cd0ca4
giautamsinha <gautamsinha1122@gmail.com>
2022-04-02 17:03:17 +0530

    postgre

--- carzone/settings.py
+++ carzone/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+# os.path.join(BASE_DIR, 'db.sqlite3'),
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+
     }
 }
 
